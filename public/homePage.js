632b09194db1de004c4bb32fffd5074ae79cad0f
Kopylov Sergey <kopylov.serge@gmail.com>
2022-02-26 22:37:11 +0300

    Получение информации о пользователе - 1

--- public/homePage.js
+++ public/homePage.js
@@ -1,3 +1,4 @@
+// Выход из личного кабинета
 
 let doLogout = (response) => {
     if (response.success === true) {
@@ -7,3 +8,17 @@ let doLogout = (response) => {
 
 const logoutBtn = new LogoutButton();
 logoutBtn.action = () => ApiConnector.logout(doLogout);
+
+
+// Получение информации о пользователе
+
+let doShowCurrentUser = (response) => {
+    if (response) {
+        console.log(response);
+        ProfileWidget.showProfile(response);
+    }
+}
+
+let User = () => ApiConnector.current(doShowCurrentUser);
+// console.log(User());
+
